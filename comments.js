6279ceb946fd646220b1cb26a3b245f383f07e01
FernandoVital <80859437+FernandoVital@users.noreply.github.com>
2025-01-21 22:31:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,62 @@
 //create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-//create a route to get comments
-
-app.get('/comments', (req, res) => {
-    res.json(comments);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const comments = require('./comments');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const query = parsedUrl.query;
+    const pathname = parsedUrl.pathname;
+    if (req.method === 'GET') {
+        if (pathname === '/comments') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(comments));
+        } else {
+            fs.readFile(path.join(__dirname, 'public', pathname), 'utf8', (err, data) => {
+                if (err) {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.end('Not Found');
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.end(data);
+                }
+            });
+        }
+    } else if (req.method === 'POST') {
+        if (pathname === '/comments') {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                const newComment = JSON.parse(body);
+                comments.push(newComment);
+                res.writeHead(201, {
+                    'Content-Type': 'application/json'
+                });
+                res.end(JSON.stringify(newComment));
+            });
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Not Found');
+        }
+    } else {
+        res.writeHead(405, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Method Not Allowed');
+    }
 });
-
-//start the server
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
